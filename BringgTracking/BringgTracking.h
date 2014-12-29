ed75b85412ff62038f818be0570ffa06c1082b50
Ilya <ilyak@Ilya-Kalinins-Mac-mini.local>
2014-12-29 11:11:27 +0200

    Initial Commit

---
+++ BringgTracking/BringgTracking.h
@@ -0,0 +1,13 @@
+//
+//  BringgTracking.h
+//  BringgTracking
+//
+//  Created by Ilya Kalinin on 12/29/14.
+//  Copyright (c) 2014 Ilya Kalinin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BringgTracking : NSObject
+
+@end
---
+++ BringgTracking/BringgTracking.m
@@ -0,0 +1,13 @@
+//
+//  BringgTracking.m
+//  BringgTracking
+//
+//  Created by Ilya Kalinin on 12/29/14.
+//  Copyright (c) 2014 Ilya Kalinin. All rights reserved.
+//
+
+#import "BringgTracking.h"
+
+@implementation BringgTracking
+
+@end

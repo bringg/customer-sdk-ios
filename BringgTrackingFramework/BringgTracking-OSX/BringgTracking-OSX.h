b47661d755c5086827423db6d92b60351c7581f8
Matan Poreh <matan@bringg.com>
2016-08-29 23:45:19 +0300

    no message

---
+++ BringgTrackingFramework/BringgTracking-OSX/BringgTracking-OSX.h
@@ -0,0 +1,32 @@
+//
+//  BringgTracking-OSX.h
+//  BringgTracking-OSX
+//
+//  Created by Porehz on 29/08/2016.
+//  Copyright © 2016 Bringg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BringgTracking-OSX.
+FOUNDATION_EXPORT double BringgTracking_OSXVersionNumber;
+
+//! Project version string for BringgTracking-OSX.
+FOUNDATION_EXPORT const unsigned char BringgTracking_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BringgTracking_OSX/PublicHeader.h>
+#import <BringgTracking_OSX/BringgGlobals.h>
+#import <BringgTracking_OSX/GGBringgUtils.h>
+#import <BringgTracking_OSX/GGCustomer.h>
+#import <BringgTracking_OSX/GGDriver.h>
+#import <BringgTracking_OSX/GGFindMe.h>
+#import <BringgTracking_OSX/GGHTTPClientManager.h>
+#import <BringgTracking_OSX/GGItem.h>
+#import <BringgTracking_OSX/GGOrder.h>
+#import <BringgTracking_OSX/GGRating.h>
+#import <BringgTracking_OSX/GGRealTimeMontior.h>
+#import <BringgTracking_OSX/GGSharedLocation.h>
+#import <BringgTracking_OSX/GGTrackerManager.h>
+#import <BringgTracking_OSX/GGWaypoint.h>
+
+
